0713ce160a317b693b3206043e353cb2afa01275
Dorian Brmbota <dorianb2@hotmail.com>
2017-10-15 20:34:32 +0200

    Add project files.

---
+++ RaupjcFirst/IntegerList.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RaupjcFirst
+{
+    public class IntegerList : IIntegerList
+    {
+        private int[] _internalStorage = new int[0];
+        public void Add(int item)
+        {
+            var newInternalStorage = new int[_internalStorage.Length + 1];
+            for (var position = 0; position < _internalStorage.Length; position++)
+            {
+                newInternalStorage[position] = _internalStorage[position];
+            }
+            newInternalStorage[_internalStorage.Length] = item;
+            _internalStorage = newInternalStorage;
+        }
+        public bool Remove(int item)
+        {
+            for (var index = 0; index < _internalStorage.Length; index++)
+            {
+                if (_internalStorage[index] == item)
+                    return RemoveAt(index);
+            }
+            return false;
+        }
+
+        public bool RemoveAt(int index)
+        {
+            if(index >= _internalStorage.Length || index < 0)
+                throw new IndexOutOfRangeException();
+            var newInternalStorage = new int[_internalStorage.Length - 1];
+            for (var position = 0; position < _internalStorage.Length; position++)
+            {
+                if(position == index)
+                    continue;
+                newInternalStorage[position > index ? position - 1 : position] = _internalStorage[position];
+            }
+            _internalStorage = newInternalStorage;
+            return true;
+        }
+
+        public int GetElement(int index)
+        {
+            if (index >= _internalStorage.Length || index < 0)
+                throw new IndexOutOfRangeException();
+            return _internalStorage[index];
+        }
+
+        public int IndexOf(int item)
+        {
+            for (var index = 0; index < _internalStorage.Length; index++)
+            {
+                if (_internalStorage[index] == item)
+                    return index;
+            }
+            throw new ArgumentException();
+        }
+
+        public int Count => _internalStorage.Length;
+
+        public void Clear()
+        {
+            _internalStorage = new int[0];
+        }
+
+        public bool Contains(int item)
+        {
+            foreach (var element in _internalStorage)
+            {
+                if (element == item)
+                    return true;
+            }
+            return false;
+        }
+
+    }
+}
---
+++ UnitTestRaupjc/UnitTest1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RaupjcFirst;
+
+namespace UnitTestRaupjc
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            IIntegerList listOfIntegers = new IntegerList();
+            
+            listOfIntegers.Add(1); // [1]
+            listOfIntegers.Add(2); // [1 ,2]
+            listOfIntegers.Add(3); // [1 ,2 ,3]
+            listOfIntegers.Add(4); // [1 ,2 ,3 ,4]
+            listOfIntegers.Add(5); // [1 ,2 ,3 ,4 ,5]
+            listOfIntegers.RemoveAt(0); // [2 ,3 ,4 ,5]
+            listOfIntegers.Remove(5); //[2 ,3 ,4]
+            Assert.AreEqual(3, listOfIntegers.Count); // 3
+            Assert.IsFalse(listOfIntegers.Remove(100));
+            Assert.ThrowsException<IndexOutOfRangeException>(() => listOfIntegers.RemoveAt(5));
+            listOfIntegers.Clear() ; // []
+            Assert.AreEqual(0, listOfIntegers.Count);
+        }
+    }
+}
